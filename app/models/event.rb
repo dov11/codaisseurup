5d498cbfbbf647c880a414968626a898e3f1a538
dov11 <ovo.dan@gmail.com>
2017-11-07 13:15:27 +0100

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,16 @@ validates :name, presence: true
 validates :description, presence: true, length: {maximum: 500}
 validates :starts_at, presence: true
 validates :ends_at, presence: true
-validates :ends_day_after_starts
+validate :ends_day_after_starts
 
-def def ends_day_after_starts
-  errors.add(:ends_at, "must be at least one day after start") if ends_at-starts_at<1
+def ends_day_after_starts
+  errors.add(:ends_at, "must be at least one day after start") if ends_at - starts_at < 1
+end
+
+after_initialize do |event|
+  event.price = event.price ? event.price : 0.0
+  event.includes_food=event.includes_food ? event.includes_food : false
+  event.includes_drinks=event.includes_drinks ? event.includes_drinks : false
 end
 
 end
