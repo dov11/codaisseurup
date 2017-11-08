1a02703e7f6b5e60f9776dfda2bea45d5fb82ea3
dov11 <ovo.dan@gmail.com>
2017-11-08 15:46:49 +0100

    add test tdd

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,27 +2,35 @@ class Event < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
 
-validates :name, presence: true
-validates :description, presence: true, length: {maximum: 500}
-validates :starts_at, presence: true
-validates :ends_at, presence: true
-validate :ends_day_after_starts
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 500}
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validate :ends_day_after_starts
 
-def ends_day_after_starts
-  if ends_at.to_i < 1*3600*24+starts_at.to_i
-    errors.add(:ends_at, "must be at least one day after start")
-    # puts "not good"
-  # else
-    # puts "ok"
+  def ends_day_after_starts
+    if ends_at.to_i < 1*3600*24+starts_at.to_i
+      errors.add(:ends_at, "must be at least one day after start")
+      # puts "not good"
+      # else
+      # puts "ok"
+    end
+
   end
 
-end
+  after_initialize do |event|
+    event.price = event.price ? event.price : 0.0
+    event.includes_food=event.includes_food ? event.includes_food : false
+    event.includes_drinks=event.includes_drinks ? event.includes_drinks : false
 
-after_initialize do |event|
-  event.price = event.price ? event.price : 0.0
-  event.includes_food=event.includes_food ? event.includes_food : false
-  event.includes_drinks=event.includes_drinks ? event.includes_drinks : false
+  end
 
-end
+  def bargain?
+    price < 30
+  end
+  
+  def self.order_by_price
+    order :price
+  end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,22 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe "validations" do
-    it "is invalid without a name" do
-      event = Event.new(name: "")
-      event.valid?
-      expect(event.errors).to have_key(:name)
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_length_of(:description).is_at_most(500) }
+    it { is_expected.to validate_presence_of(:ends_at) }
+    it { is_expected.to validate_presence_of(:starts_at) }
+    describe "#bargain?" do
+      let(:bargain_event) { create :event, price: 20 }
+      let(:non_bargain_event) { create :event, price: 200 }
 
-    it "is invalid without a description" do
-      event = Event.new(description: nil)
-      event.valid?
-      expect(event.errors).to have_key(:description)
+      it "returns true if the price is smaller than 30 EUR" do
+        expect(bargain_event.bargain?).to eq(true)
+        expect(non_bargain_event.bargain?).to eq(false)
+      end
     end
+    describe ".order_by_price" do
+   let!(:event1) { create :event, price: 100 }
+   let!(:event2) { create :event, price: 200 }
+   let!(:event3) { create :event, price: 50 }
 
-    it "is invalid with a listing name longer than 500 characters" do
-      event = Event.new(description: Faker::Lorem.characters(510))
-      event.valid?
-      expect(event.errors).to have_key(:description)
-    end
+   it "returns a sorted array of rooms by prices" do
+     # note that they should not come out in the order that they were created
+     expect(Event.order_by_price).to eq([event3, event1, event2])
+   end
+ end
   end
 end
