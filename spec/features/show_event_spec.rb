9c78f110ecb0f7f81aa821ea3536f0d5d7351696
dov11 <ovo.dan@gmail.com>
2017-11-08 14:27:33 +0100

    added start date

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -10,6 +10,7 @@ describe "Viewing an individual event" do
     expect(page).to have_text("Includes drinks!")
     expect(page).to have_text(event.description)
     expect(page).to have_text(event.name)
+    expect(page).to have_text(event.starts_at.strftime("On %d/%m/%Y"))
     # expect(page).to have_text(room.bathroom_count)
     # expect(page).to have_text(room.listing_name)
     # expect(page).to have_text(room.description)
