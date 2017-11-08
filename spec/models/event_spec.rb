a7262ad4ac6c1a4fe357650815b3b52e4af89b03
dov11 <ovo.dan@gmail.com>
2017-11-08 16:44:12 +0100

    add one-to-many and many-to-many tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -17,14 +17,36 @@ RSpec.describe Event, type: :model do
       end
     end
     describe ".order_by_price" do
-   let!(:event1) { create :event, price: 100 }
-   let!(:event2) { create :event, price: 200 }
-   let!(:event3) { create :event, price: 50 }
+      let!(:event1) { create :event, price: 100 }
+      let!(:event2) { create :event, price: 200 }
+      let!(:event3) { create :event, price: 50 }
 
-   it "returns a sorted array of rooms by prices" do
-     # note that they should not come out in the order that they were created
-     expect(Event.order_by_price).to eq([event3, event1, event2])
-   end
- end
+      it "returns a sorted array of rooms by prices" do
+        # note that they should not come out in the order that they were created
+        expect(Event.order_by_price).to eq([event3, event1, event2])
+      end
+    end
   end
+  describe "association with user" do
+    let(:user) { create :user }
+
+    it "belongs to a user" do
+      event = user.events.build(name: "Partey")
+
+      expect(event.user).to eq(user)
+    end
+  end
+  describe "association with category" do
+  let(:event) { create :event }
+
+  let(:category1) { create :category, name: "Bright", events: [event] }
+  let(:category2) { create :category, name: "Clean lines", events: [event] }
+  let(:category3) { create :category, name: "A Man's Touch", events: [event] }
+
+  it "has categories" do
+    expect(event.categories).to include(category1)
+    expect(event.categories).to include(category2)
+    expect(event.categories).to include(category3)
+  end
+end
 end
